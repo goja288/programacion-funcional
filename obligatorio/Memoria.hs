809d0bd4cd38e2deda03cfddde2d196bdca851b2
goja288 <goja288@gmail.com>
2015-05-23 18:38:11 -0300

    Cambios en  Memoria

--- obligatorio/Memoria.hs
+++ obligatorio/Memoria.hs
@@ -1,11 +1,22 @@
 module Memoria where
 
+import Data.Maybe
 
 type Memoria = (String,String)
 
-newMemoria :: String -> String -> String
+newMemoria :: String -> String -> Memoria
 newMemoria clave valor = (clave,valor)
 
-getClave :: String -> [Memoria] -> String
-getClave clave memoria
+newMemoriaLista :: [Memoria]
+newMemoriaLista = []
 
+-- TODO hacer el chequeo de errores 
+getValor :: String -> [Memoria] -> String
+getValor clave memoria = fromJust (lookup clave memoria)
+
+addElem :: Memoria -> [Memoria] -> [Memoria]
+addElem mem [] = mem:[]
+addElem mem ((x,y):xs)
+	| x == fst mem = mem:xs -- Actualizo si existe 
+	| xs == [] = (x,y):(mem:xs) -- Agrego si no esta
+	| x /= fst mem = (x,y):(addElem mem xs) -- Llamado recursivo
--- obligatorio/RunAwki.hs
+++ obligatorio/RunAwki.hs
@@ -1,11 +1,12 @@
 module RunAwki(runAwki) where
 
 import AwkiSA
+import Memoria
 
 runAwki :: AwkiProg -> String -> String
 runAwki (AwkiProg [(BEGIN,Empty)]) _ = "ola no es lo mismo que Hola"
 -- '{ print }'
-runAwki (AwkiProg [(Pat (Lit 1),Sequence [Print [Field (Lit 0)]])]) a = a -- '{ print }'
+runAwki ( AwkiProg [(Pat (Lit 1),Sequence [Print [Field (Lit 0)]])] ) a = a -- '{ print }'
 
 runAwki _ _ = "a bu"
 
