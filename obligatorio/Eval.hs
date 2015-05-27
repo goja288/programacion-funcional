e2abaf955052f0930155b10d193c1cd98c8b7073
Cristian Bauza <cristian.bauza.silva@gmail.com>
2015-05-27 20:21:51 -0300

    Cambios en el evaluador

--- obligatorio/Eval.hs
+++ obligatorio/Eval.hs
@@ -65,22 +65,25 @@ eval m (Op1 Plus a) = (m, snd (eval m a)) -- VER ACA ???????????????????????????
 
 eval m (Op1 Minus a) = (m, ((snd (eval m a)) * (-1)))
 
---eval m (Op1 Minus a) = (m, (a * (-1))
+eval m (Op1 Not a) 
+	| toBool (snd (eval m a)) == False = (m, Num 1)
+	| otherwise = (m, Num 0)	
+-- eval [("$1", "10")] (Op1 Not (Op2 Gt (Var "$1") (Lit (Num 1))))
+-- eval [("$1", "10")] (Op1 Not (Op2 Lt (Var "$1") (Lit (Num 1))))
 
--- DEFINIMOS LAS ASIGNACIONES DE VARIABLES A LAS ASIGNACIONES DE VARIABLES ---------------------------------------------------
+-- DEFINIMOS LAS ASIGNACIONES DE VARIABLES -----------------------------------------------------------------------------------
 eval m (Assign s a) = (addElem (s, (show (snd (eval m a)))) m, snd (eval m a))
 
 -- DEFINIMOS LAS ACUMULACIONES -----------------------------------------------------------------------------------------------
 eval m (Accum b s a) = ((addElem (s, (show (snd (eval m (Op2 b (Var s) a))))) m), (snd (eval m (Op2 b (Var s) a))))
+-- eval [("$1", "10")] (Accum Add "$1" (Lit (Num 1))) // Deberia Guardar 11 y devolver 11
 
 -- DEFINIMOS LAS PP ----------------------------------------------------------------------------------------------------------
 eval m (PP True True s) = (addElem (s, show (snd (eval m (Op2 Add (Var s) (Lit 1))))) m, snd (eval m (Op2 Add (Var s) (Lit 1))))
---eval m (PP False True s) =  
---	let pre = snd (eval m (Var s))
---		postInc = snd (eval m (Op2 Add (Var s) (Lit 1)))
---	in (addElem (s, show postInc) m, pre)
-					  --in (m, Num 1) --
-
+eval m (PP False True s) =  (addElem (s, show (snd (eval m (Op2 Add (Var s) (Lit 1))))) m, snd (eval m (Var s)))
+eval m (PP True False s) = (addElem (s, show (snd (eval m (Op2 Add (Var s) (Lit (-1)))))) m, snd (eval m (Op2 Add (Var s) (Lit (-1)))))
+eval m (PP False False s) =  (addElem (s, show (snd (eval m (Op2 Add (Var s) (Lit (-1)))))) m, snd (eval m (Var s)))
+-- eval [("$1", "10")] (PP False False "$1") // Deberia guardar 9 y devolver 10
 
 
 	
