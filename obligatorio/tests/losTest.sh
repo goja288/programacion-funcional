5a37c8ae47b0aca27833c0eaf20ad86af35ae2a2
Gonzalo Javiel Mimbacas - ENS <gonzalo.javiel@pcunix128.fing.edu.uy>
2015-06-02 20:05:33 -0300

    Borro para entregar

--- obligatorio/tests/losTest.sh
+++ obligatorio/tests/losTest.sh
@@ -83,7 +83,7 @@ progs[69]='(x = 1) && (x = 7) {print x}'
 progs[70]='BEGIN {i = 4; while (i != 5) {i--; print $i}}'
 progs[71]='BEGIN {print "Begin"; exit; var = 5}; 1 {var--; print var}; END {print var; exit; print "End"}'
 
-#progs[72]='BEGIN {myVar = 72}; ($3 == 34) && ($4 == 34) {print $1}; ($3 < 87) || ($4 <315) {i = 22; while (i < 550) {print $1; i++}; print myVar; var2 += (517 * ((myVar || 45) + (var && (3 < 7))))}; $1 == "games" {print myVar, var, (mayVar+=(var = var)); var3++}; $1 != "sebarl" {print "blah", "bleh", (5+7),(537-2+3*3)}; 1 {a = 3; while (a<50) {h = 5; do{for(n = h; n<20;n+=3){print a, h, n, $(a*h*n)};h++} while(h<14); a++}}; $2 == "x" {if (var3211) {if (1) {print "blah"} else {print "blehman"}} else { if ("blah") {print "ohyeahman"} else {print "ohnoes!"}}}; END {blahman557 = "SPAMMASTER"; exit}'
+progs[72]='BEGIN {myVar = 72}; ($3 == 34) && ($4 == 34) {print $1}; ($3 < 87) || ($4 <315) {i = 22; while (i < 550) {print $1; i++}; print myVar; var2 += (517 * ((myVar || 45) + (var && (3 < 7))))}; $1 == "games" {print myVar, var, (mayVar+=(var = var)); var3++}; $1 != "sebarl" {print "blah", "bleh", (5+7),(537-2+3*3)}; 1 {a = 3; while (a<20) {h = 5; do{for(n = h; n<20;n+=3){print a, h, n, $(a*h*n)};h++} while(h<14); a++}}; $2 == "x" {if (var3211) {if (1) {print "blah"} else {print "blehman"}} else { if ("blah") {print "ohyeahman"} else {print "ohnoes!"}}}; END {blahman557 = "SPAMMASTER"; exit}'
 
 # comparacion de strings
 progs[153]='{var = "pepe"; var2 = "es"; var3 = (var && var2); print var3 }'
