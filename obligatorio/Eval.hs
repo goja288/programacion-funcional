818f59c251e8f039230ef008029982b8e93d0042
Gonzalo Javiel Mimbacas - ENS <gonzalo.javiel@pcunix128.fing.edu.uy>
2015-06-04 13:50:08 -0300

    Elimino la entrega vieja

--- obligatorio/Eval.hs
+++ obligatorio/Eval.hs
@@ -17,7 +17,7 @@ eval m (Lit v)
 
 eval m (Var s)
 	| evalError m  == True = (m, (Num 0))
-	| otherwise = (m, (toValueStr (Map.lookup s m))) -- @INFO: REVISAR
+	| otherwise = (m, (toValueStr (Map.lookup s m)))
 
 -- DEFINIMOS LAS EXPRESIONES DE LAS OPERACIONES DOS PARAMETROS ---------------------------------------------------------------
 eval m (Op2 Add a b)
@@ -63,7 +63,7 @@ eval m (Op2 Div a b)
 		let 
 		dupla1 = eval m a
 		dupla2 = eval (fst dupla1) b
-		in (Map.insert "-1" (Str "Error: Division por cero\n") (fst dupla2), (Num 0))
+		in (Map.insert "-1" (Str "ERROR\n") (fst dupla2), (Num 0))
 	
 eval m (Op2 Mod a b)
 	| evalError m  == True = (m, (Num 0))
@@ -78,7 +78,7 @@ eval m (Op2 Mod a b)
 		let 
 		dupla1 = eval m a
 		dupla2 = eval (fst dupla1) b
-		in (Map.insert "-1" (Str "Error: Division por cero\n") (fst dupla2), (Num 0))
+		in (Map.insert "-1" (Str "ERROR\n") (fst dupla2), (Num 0))
 
 -- VER ALGUN EJEMPLO COMO HIZO EL RESTO CON EL TEMA DE LAS COMPARACIONES, QUE DEVUELVEN?
 eval m (Op2 Lt a b) 
@@ -241,7 +241,7 @@ eval m (Assign s a)
 				in if (evalError (fst dupla)) then
 					dupla
 				else
-					(Map.insert s (snd dupla) (fst dupla), snd dupla) -- @INFO cambio
+					(Map.insert s (snd dupla) (fst dupla), snd dupla)
 
 -- DEFINIMOS LAS ACUMULACIONES -----------------------------------------------------------------------------------------------
 eval m (Accum b s a) 
@@ -249,7 +249,7 @@ eval m (Accum b s a)
 	| evalError (fst (eval m a)) == True = eval m a
 	| otherwise = 
 		let dupla1 = eval (fst (eval m a)) (Op2 b (Var s) a)
-		in (Map.insert s (snd dupla1) (fst dupla1), (snd dupla1)) -- @INFO cambio
+		in (Map.insert s (snd dupla1) (fst dupla1), (snd dupla1))
 
 -- DEFINIMOS LAS PP ----------------------------------------------------------------------------------------------------------
 eval m (PP True True s) 
@@ -279,7 +279,7 @@ eval m (PP False False s)
 -- DEFINIMOS LA EXPESION "FIELD EXPR" ------------------------------------------------------------------------------------------
 eval m (Field a)
 	| evalError m  == True = (m, (Num 0))
-	| (toBool (snd (eval (fst (eval m a)) (Op2 Lt (Lit (snd (eval m a))) (Lit (Num 0)))))) == True = (Map.insert "-1" (Str "Error: Variable Negativa\n") (fst (eval (fst (eval m a)) (Op2 Lt (Lit (snd (eval m a))) (Lit 0)))), Num 0) -- chequeo variables negativa
+	| (toBool (snd (eval (fst (eval m a)) (Op2 Lt (Lit (snd (eval m a))) (Lit (Num 0)))))) == True = (Map.insert "-1" (Str "ERROR\n") (fst (eval (fst (eval m a)) (Op2 Lt (Lit (snd (eval m a))) (Lit 0)))), Num 0) -- chequeo variables negativa
 	| otherwise = eval (fst (eval m a)) (Var (show (snd (eval m a))))
 				  
 
@@ -292,7 +292,6 @@ toString maybeValue = case maybeValue of
   Just value -> value
   Nothing    -> ""
 
--- @INFO: AGREGO
 toValueStr :: Maybe Valor -> Valor
 toValueStr maybeValue = case maybeValue of
   Just value -> value
@@ -307,8 +306,3 @@ toValueNum maybeValue = case maybeValue of
 evalError :: Map String Valor -> Bool
 evalError m = Map.member "-1" m 
 
--- debugImprimirMemoria :: Map String Valor -> String
--- debugImprimirMemoria memoria = "\n\t-------------\n\t" ++ (foldl1 (++) (map (++"\n") (map juntarDupla (Map.toList memoria)))) ++ "\n\t-------------\n"
-
--- juntarDupla :: (String, String) -> String
--- juntarDupla (a,b) = a ++ "\t" ++b
